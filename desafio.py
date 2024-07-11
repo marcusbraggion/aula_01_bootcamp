ab6c16bd9be0d42b7695107024a3b6ffdf5b4991
Marcus Bragion <viniciusbragion@outlook.pt>
2024-07-10 23:16:40 -0300

    chore: remover arquivo desafio.py e exemplo_01.py

--- desafio.py
+++
@@ -1,9 +0,0 @@
-# Desafio do dia: Cálculo de Bônus com Entrada do Usuário
-# Escreva um programa em Python que solicita ao usuário para digitar seu nome, o valor do seu salário mensal e o valor do bônus que recebeu.
-#O programa deve, então, imprimir uma mensagem saudando o usuário pelo nome e informando o valor do salário em comparação com o bônus recebido.
-
-# Receber o nome do usuario
-nome = str(input("Digite seu nome: "))
-salario = float(input("Digite o valor do seu salario: "))
-bonus = float(input("Digite o valor do bônus: "))
-print(f"Olá {nome}, o seu valor bônus foi de {1000 + ( salario * bonus)}")
--- exemplo_01.py
+++
@@ -1,3 +0,0 @@
-# Crie um programa de usuário que recebe dois valores e retorne a soma
-val = int(input("Digite o primeiro número: ")) + int(input("Digite o segundo número: "))
-print(val)
